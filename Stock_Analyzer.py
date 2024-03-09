2e4f447723cb1201a192d58ac76044050da1cc94
Liam Crowe <20093470@mail.wit.ie>
2024-03-09 22:24:32 +0000

    All data is now combined into one excel spreadsheet

--- Stock_Analyzer.py
+++ Stock_Analyzer.py
@@ -1,18 +1,14 @@
 import yfinance as yf
 import pandas as pd
+import os
 
 """ 
-        Install openpyxl if not already installed.
+    Install openpyxl if not already installed.
     Can cause error with sending data to excel fill if not installed
     You can install it using: pip install openpyxl
 """
 
 aapl = yf.Ticker("AAPL")
-
-# Fetch quarterly balance sheet data
-balance_sheet_df = aapl.quarterly_balance_sheet
-# Save the data to an Excel file
-balance_sheet_df.to_excel("Apple_data_quarterly_balance_sheet.xlsx")
 
 # Fetch dividends data and parse 'Date' column during retrieval
 dividends_df = aapl.dividends.reset_index()
@@ -33,10 +29,29 @@ income_statement_df = aapl.quarterly_income_stmt
 # Save the data to an Excel file
 income_statement_df.to_excel("Apple_data_quarterly_income_statement.xlsx")
 
-df = pd.read_excel("Apple_data_quarterly_balance_sheet.xlsx")
-print(df)
+# Fetch quarterly balance sheet data
+balance_sheet_df = aapl.quarterly_balance_sheet
+# Save the data to an Excel file
+balance_sheet_df.to_excel("Apple_data_quarterly_balance_sheet.xlsx")
+
+#df = pd.read_excel("Apple_data_quarterly_balance_sheet.xlsx")
+#print(df)
 
 #Add all the data into a large spreadsheet
+folder_path = 'D:\CodeProjects\Python\Stock-Analyzer-With-YahooFinance'
+files = [f for f in os.listdir(folder_path) if f.endswith('.xlsx')]
+
+dataFrames = []
+for file in files:
+    df = pd.read_excel(os.path.join(folder_path, file))
+    dataFrames.append(df)
+
+merged_df = pd.concat(dataFrames)
+
+output_path = 'D:\CodeProjects\Python\Stock-Analyzer-With-YahooFinance'
+merged_df.to_excel(os.path.join(output_path, 'data_combined.xlsx'), index=False)
+
+
 """
 Format the data correctly
 Widen the columns
